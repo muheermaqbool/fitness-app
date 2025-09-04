ac490b2a4572fb44122b3e6bf8de1d569fcdeb20
Muheer Maqbool <muheermaqbool6@gmail.com>
2025-09-04 16:54:24 +0530

    added image&proicon to homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,32 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 import { SafeAreaView as SafeAreaViewContext } from "react-native-safe-area-context";
+import Ionicons from "@expo/vector-icons/Ionicons";
 export default function HomeScreen() {
   return (
-    <SafeAreaViewContext>
-      <SafeAreaView>
-        <View>
-          <Text>HomeScreen</Text>
-          {/* Pro/Upgrade-button */}
-          
-          {/* image */}
+    <SafeAreaViewContext className="flex-1 bg-gray-300 relative">
+        <ScrollView>
+          <View className=" p-7">
+          <TouchableOpacity className="p-5 absolute top-0 right-0 items-center">
+            <Ionicons name="person-circle-sharp" size={30} color='#22C55E' />
+            <Text className='text-green-500'>PRO</Text>
+          </TouchableOpacity>
+          <Image
+            source={require("../public/images/logo.png")}
+            className="w-full h-64"
+          />
 
           {/* Action Components */}
-        </View>
-      </SafeAreaView>
+          </View>
+        </ScrollView>
     </SafeAreaViewContext>
   );
 }
-
-const styles = StyleSheet.create({});
